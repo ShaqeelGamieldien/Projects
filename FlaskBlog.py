2c214ee189f3fde9501881e599dd3aba37028082
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-08-29 13:14:35 +0200

    Added dummy data for testing

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -3,6 +3,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+ {
+  'author' : 'Asura Samsara'
+  'title' : 'Blog post 1'
+  'content': 'Oonga Boonga'
+  'date_poste': 'April 25, 2018'
+ },
+ {
+  'author' : 'Rogal Dorne'
+  'title' : 'Praetorian of Man'
+  'content': 'Build walls'
+  'date_poste': 'May 20, 2018'
+ }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -10,7 +25,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return "<h1>ABOUT page</h1>"
+    return render_template('about.html')
 
 
 
