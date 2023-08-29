a6e84613eddd591409d205c4794633ba80913f6d
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-08-29 13:03:08 +0200

    Added about Route

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Home Page</h1>"
 
+@app.route("/about")
+def about():
+    return "<h1>ABOUT page</h1>"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
