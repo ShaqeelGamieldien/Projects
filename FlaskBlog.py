1fe51b768cfca442cc84a5266b220c2df6c1bae4
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-08-31 14:42:30 +0200

    Added dynamic urls to template

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -43,10 +43,12 @@ def register():
 @app.route("/login", methods=['GET','POST'])
 def Login():
     form= LoginForm()
-        if form.validate_on_submit():
-
-            flash(f'Account created for {form.username.data}!', 'success')
+    if form.validate_on_submit():
+        if form.email.data== 'admin@blog.com' and form.password.data == 'password':
+            flash('Logged in successfully!', 'success')
             return redirect(url_for('home'))
+        else:
+                flash('Log in unsuccessful', 'danger')
     return render_template('login.html',title='Login', form=form)
 
 
