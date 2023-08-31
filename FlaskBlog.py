37d6d376b6b758379cb6931088c09cfad9eeb9c4
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-08-31 15:10:34 +0200

    Addition of Comments to Code

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -24,7 +24,7 @@ class Post(db.Model):
     id = db.Column(db.String, primary_key = True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime , nullable=False, default = datetime.utcnow)
-
+    content =db.Column(db.Text, nullable=False) 
 
 posts = [
  {
