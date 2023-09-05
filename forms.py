a60ce8f6a49093f47e24ca2e6b1ab24495d84f83
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-09-05 12:25:41 +0200

    added Logic for unique data validation

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from flask_wtf import *
 from wtforms import *
 from wtforms.validators import *
 import email_validator
-
+from FlaskBlog.models import User
 class RegistrationForm(FlaskForm):
         username=StringField('Username',
                             validators = [DataRequired(), Length(min =2, max =20 )]
@@ -15,6 +15,20 @@ class RegistrationForm(FlaskForm):
 
         submit=SubmitField('Sign Up')
 
+        def validate_field(self,field):
+            if True:
+                raise ValidationError('Validation Message')
+
+
+        def validate_username(self,username):
+            user=User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one')
+
+        def validate_email(self,email):
+            email=User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one')
 
 
 class LoginForm(FlaskForm):
