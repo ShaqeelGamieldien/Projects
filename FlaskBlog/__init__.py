85366871043e0bef9fbe00a88c25fcb189e488d9
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-09-05 11:50:44 +0200

    Addition of init package folder

---
+++ FlaskBlog/__init__.py
@@ -0,0 +1,12 @@
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm  # Make sure the import paths are correct
+from datetime import datetime  # Correct the import of datetime
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '40bfaaf7cf15c237128ef5ae12c62e85'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Correct the config key name
+
+db = SQLAlchemy(app)
