52413c6632d6f23704e28372c1dd09bba53977ab
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-09-06 12:10:04 +0200

    indent

--- FlaskBlog/routes.py
+++ FlaskBlog/routes.py
@@ -56,5 +56,5 @@ def login():
             login_user(user,remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-        flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
