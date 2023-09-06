195343d728ecd151ac0045a1a0f0ba24619e6ea7
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-09-06 12:22:18 +0200

    Adding account window

--- FlaskBlog/routes.py
+++ FlaskBlog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request
 from FlaskBlog import app, db, bcrypt
 from FlaskBlog.forms import *
 from FlaskBlog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 
 posts = [
@@ -34,7 +34,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -48,7 +49,8 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -58,3 +60,17 @@ def login():
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+
+def logout():
+    logout_user()
+    flash('You have logged out.')
+    return redirect(url_for('home'))
+
+
+
+@app.route('/account')
+def account():
+        return render_template('account.html', title='Account', form=form)
