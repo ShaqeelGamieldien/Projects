0d422d5581d86612ca392ca7c1a6bf64f1084422
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-09-07 04:44:24 +0200

    Adding update to profile pic logic

--- FlaskBlog/forms.py
+++ FlaskBlog/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import *
 from wtforms import *
 from wtforms.validators import *
 import email_validator
+from FlaskBlog.models import *
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
         username=StringField('Username',
@@ -14,6 +16,15 @@ class RegistrationForm(FlaskForm):
 
 
         submit=SubmitField('Sign Up')
+        def validate_username(self, username):
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+        def validate_email(self, email):
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 
@@ -23,12 +34,13 @@ class LoginForm(FlaskForm):
         password = PasswordField('Confirm Password',    validators = [DataRequired()])
         remember= BooleanField('Remember Me')
         submit=SubmitField('Sign Up')
+
 class UpdateForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-
+    picture =FileField('Update Profile', validators[FileAllowed[('jpg','png')]])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
