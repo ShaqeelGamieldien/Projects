7501b937ffaef73e6062f9abcd5c99d08519ef6c
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-09-07 05:01:44 +0200

    Adding logic

--- FlaskBlog/routes.py
+++ FlaskBlog/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template, url_for, flash, redirect, request
 from FlaskBlog import app, db, bcrypt
-
+import secrets
 from FlaskBlog.forms import *
 from FlaskBlog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
+import os
 
 posts = [
     {
@@ -72,7 +72,14 @@ def logout():
 
 
 def save_picture(form_picture):
-    
+    random_hex= secrets.token_hex(8)
+    _ , f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex+f_ext
+    picture_path =os.path.join(app.root_path,'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+
 
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
@@ -80,7 +87,7 @@ def account():
     form = UpdateForm()
     if form.validate_on_submit():
         if form.picture.data:
-
+            save_picture(form.picture.data)
         current_user.username =form.username.data
         current_user.email =form.email.data
         db.session.commit()
