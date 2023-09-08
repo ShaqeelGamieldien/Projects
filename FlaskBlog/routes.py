01affb0a9ef52ef3bed21c40985b6a67c956c845
Shaqeel <shaqeel.gamieldien@gmail.com>
2023-09-08 12:48:15 +0200

    Added the logic to update Posts

--- FlaskBlog/routes.py
+++ FlaskBlog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template,abort, url_for, flash, redirect, request
 from FlaskBlog import app, db, bcrypt
 import secrets
 from FlaskBlog.forms import *
@@ -105,11 +105,28 @@ def new_post():
         flash('Your post has been created', 'success')
         return redirect(url_for('home'))
 
-    return render_template('create_post.html', title='New Post',  form= form)
-
+    return render_template('create_post.html', title='New Post',  form= form, legend= 'New Post')
 
 
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title,  post= post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    form.title.data = post.title
+    form.content.data = post.content
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content= form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+    return render_template('create_post.html', title='Update Post',  form= form, legend= 'Update Post')
